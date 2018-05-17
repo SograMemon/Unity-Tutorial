8f3bcb1930734210d720a63d730660fab55a2d49
Sogra <sogra.memon@dal.ca>
2018-05-17 16:05:11 -0300

    Tutorial 1- set hours rotation

--- Tutorial 1.0/Assets/Clock.cs
+++ Tutorial 1.0/Assets/Clock.cs
@@ -2,10 +2,15 @@
 using UnityEngine;
 public class Clock : MonoBehaviour{
 
+	const float degreePerHour = 30f;
+
 	public Transform hoursTransform, minutesTransform, secondsTransform;
 
 	void Awake () {
-		Debug.Log (DateTime.Now);
+		hoursTransform.localRotation=
+		Quaternion.Euler(0f, DateTime.Now.Hour * degreePerHour, 0f);
+
+
 	
 	}
 
