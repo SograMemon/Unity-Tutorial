60977485927c7c3fbe50304d579a5039069c6f7d
Sogra <sogra.memon@dal.ca>
2018-05-17 16:12:00 -0300

    Tutorial 1- Set rotation of minutes and seconds hand

--- Tutorial 1.0/Assets/Clock.cs
+++ Tutorial 1.0/Assets/Clock.cs
@@ -2,13 +2,21 @@
 using UnityEngine;
 public class Clock : MonoBehaviour{
 
-	const float degreePerHour = 30f;
+	const float degreePerHour = 30f,
+				degreePerMinute = 6f,
+				degreePerSecond = 6f;
 
 	public Transform hoursTransform, minutesTransform, secondsTransform;
 
 	void Awake () {
 		hoursTransform.localRotation=
 		Quaternion.Euler(0f, DateTime.Now.Hour * degreePerHour, 0f);
+
+		minutesTransform.localRotation=
+			Quaternion.Euler(0f, DateTime.Now.Minute * degreePerMinute, 0f);
+
+		secondsTransform.localRotation=
+			Quaternion.Euler(0f, DateTime.Now.Second * degreePerSecond, 0f);
 
 
 	
