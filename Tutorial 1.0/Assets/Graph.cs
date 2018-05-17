def7134cb443793ecee3ea35d1463a41b3035c75
Sogra <sogra.memon@dal.ca>
2018-05-17 17:58:57 -0300

    Tutorial 2 - Row of ten cubes

--- Tutorial 1.0/Assets/Graph.cs
+++ Tutorial 1.0/Assets/Graph.cs
@@ -7,7 +7,15 @@ public class Graph : MonoBehaviour {
 	public Transform pointPrefab;
 
 	void Awake() {
-		Instantiate (pointPrefab);
+
+		int i=0;
+		while(i<10) {
+			i = i + 1;
+			Transform point= Instantiate (pointPrefab);
+			point.localPosition = Vector3.right*i;
+		}
+
+
 
 	}
 }
