f69c571eaadac0500dfd3fd776f5f0d215113e13
Sogra <sogra.memon@dal.ca>
2018-05-17 17:09:29 -0300

    Tutorial 1 - Completed both continuous and discrete rotations

--- Tutorial 1.0/Assets/Clock.cs
+++ Tutorial 1.0/Assets/Clock.cs
@@ -8,19 +8,48 @@ public class Clock : MonoBehaviour{
 
 	public Transform hoursTransform, minutesTransform, secondsTransform;
 
-	void Awake () {
+	public bool Continuous;
+
+	void UpdateContinuous () {
+
+		TimeSpan time = DateTime.Now.TimeOfDay;
 		hoursTransform.localRotation=
-		Quaternion.Euler(0f, DateTime.Now.Hour * degreePerHour, 0f);
+			Quaternion.Euler(0f, (float)time.TotalHours * degreePerHour, 0f);
 
 		minutesTransform.localRotation=
-			Quaternion.Euler(0f, DateTime.Now.Minute * degreePerMinute, 0f);
+			Quaternion.Euler(0f, (float)time.TotalMinutes * degreePerMinute, 0f);
 
 		secondsTransform.localRotation=
-			Quaternion.Euler(0f, DateTime.Now.Second * degreePerSecond, 0f);
+			Quaternion.Euler(0f, (float)time.TotalSeconds * degreePerSecond, 0f);
 
 
 	
 	}
 
+	void UpdateDiscrete () {
+
+		DateTime time = DateTime.Now;
+		hoursTransform.localRotation=
+			Quaternion.Euler(0f, time.Hour * degreePerHour, 0f);
+
+		minutesTransform.localRotation=
+			Quaternion.Euler(0f, time.Minute * degreePerMinute, 0f);
+
+		secondsTransform.localRotation=
+			Quaternion.Euler(0f, time.Second * degreePerSecond, 0f);
+
+
+
+	}
+
+	void Update() {
+		if (Continuous) {
+			UpdateContinuous ();
+		}
+		else{
+			UpdateDiscrete ();
+		}
+
+}
 }
 
