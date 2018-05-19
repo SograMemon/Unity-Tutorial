e3bd5e5beb2381386ca1b4f4af82897f7df810c6
Sogra <sogra.memon@dal.ca>
2018-05-19 14:27:54 -0300

    Tutorial 2 - y axis = 0

--- Tutorial 1.0/Assets/Graph.cs
+++ Tutorial 1.0/Assets/Graph.cs
@@ -8,10 +8,17 @@ public class Graph : MonoBehaviour {
 
 	void Awake() {
 
+		Vector3 scale = Vector3.one / 5f;
+		Vector3 position;
+		position.y = 0f;
+		position.z = 0f;
 		for(int i=0;i<10;i++) {
 			
 			Transform point= Instantiate (pointPrefab);
-			point.localPosition = Vector3.right*i;
+			position.x = (i + 0.5f) / 5f - 1f;
+			point.localPosition = position;
+			point.localScale = scale;
+
 
 		}
 
